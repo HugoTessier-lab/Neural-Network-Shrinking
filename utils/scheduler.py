6e6fe77aa5492864a1a8755e592bc3b503813912
htessier <hugo.tessier@imt-atlantique.fr>
2022-03-11 13:55:59 +0100

    Fixes typo in scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -4,7 +4,7 @@ import math
 
 def get_scheduler(args, optimizer):
     if args.scheduler == 'multistep':
-        return torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[args.epochs // 2, 3 * (args.epochs // 3)])
+        return torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[args.epochs // 3, 2 * (args.epochs // 3)])
     elif args.scheduler == 'poly':
         def poly_schd(e):
             return math.pow(1 - e / args.epochs, args.poly_exp)
